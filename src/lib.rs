99895886bd25eebb5b926302baa1503754d4ed99
anonymoususenix <usenix_2025_for_ae@outlook.com>
2025-01-21 23:11:49 +0800

    remove draft files

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,8 @@
 pub mod commit;
 pub mod conv;
 pub mod conv_padding;
-pub mod conv_padding_old;
 pub mod maxpool;
 pub mod relu;
-pub mod relu_old;
 
 use ark_bn254::{Bn254, Fr as ScalarField, FrConfig};
 use ark_ec::pairing::Pairing;
